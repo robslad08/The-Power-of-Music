f934b40dc8c53b7246253d819341f1a3e2303d9d
robslad08 <136631991+robslad08@users.noreply.github.com>
2023-06-27 11:13:11 +0700

    Improve error handling

---
+++ xylophone_piano_yellow.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+// The Power of Music Function
+func powerOfMusic() {
+	fmt.Println("MUSIC IS POWERFUL")
+	fmt.Println("Music can make us feel emotion, it can make us laugh and cry. It can move us and inspire us.")
+	fmt.Println("Music can transport us to a different place and time. Music can bring people together and unity.")
+	fmt.Println("It has the power to heal and bring peace and solace. It can soothe the soul and bring joy when feeling down.")
+	fmt.Println("It can bring back old memories and create new ones. It lets us express ourselves in ways we may not be able to do with words.")
+	fmt.Println("Music is a universal language that everyone understands. It is powerful and its effects can be felt not only in our lives but in our communities as well.")
+}
+
+func main() {
+	powerOfMusic()
+}
