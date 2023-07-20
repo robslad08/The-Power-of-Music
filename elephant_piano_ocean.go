3cd321fbd9788441345bb9faf73022c9c362c8fc
robslad08 <136631991+robslad08@users.noreply.github.com>
2023-07-20 15:28:50 +0700

    Improve code efficiency

---
+++ elephant_piano_ocean.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("The Power of Music")
+
+	// Line 1: 
+	fmt.Println("Music has the power to move us in ways nothing else can.")
+
+	// Line 2: 
+	fmt.Println("It brings us together, inspires us, gives us hope, and helps us to create memories that last a lifetime.")
+
+	// Line 3: 
+	fmt.Println("The power of music is a beautiful thing, and it's something we can all appreciate and enjoy.")
+
+	// Line 4: 
+	fmt.Println("Whether it’s jazz, pop, hip-hop, classical, rock, or something else, the power of music is something that can transcend barriers of language, race, and culture.")
+
+	// Line 5: 
+	fmt.Println("It has the ability to make us feel emotions that words cannot express.")
+
+	// Line 6: 
+	fmt.Println("Music has the ability to take us back to a different time and place in our lives, and evoke a wide range of emotions from joy, to nostalgia, to sorrow.")
+
+	// Line 7: 
+	fmt.Println("In moments of sadness, music can be a source of comfort, bringing us hope and strength to carry on.")
+
+	// Line 8: 
+	fmt.Println("In times of celebration, music can lift our spirits and bring us joy.")
+
+	// Line 9: 
+	fmt.Println("Perhaps most importantly, music can bring people together to share an experience and build connections." )
+
+	// Line 10: 
+	fmt.Println("It can be a unifying force for humanity, helping us to appreciate the diversity of cultures and perspectives in the world.")
+
+	// Line 11: 
+	fmt.Println("No matter the genre, music has the power to bring people together and create a powerful bond through its magical ability to inspire, motivate, and uplift us.")
+
+	// Line 12: 
+	fmt.Println("So if you ever need a reminder of the power of music, just close your eyes and let it take you away.")
+
+	// Line 13: 
+	fmt.Println("Because no matter how you feel, there’s always a song that can make it better.")
+
+	// Line 14: 
+	fmt.Println("So don’t forget to turn up the volume, and let the music take you away.")
+
+	// Line 15: 
+	fmt.Println("Remember, the power of music is undeniable. So crank up your favorite tunes and dance the night away!")
+
+	// Line 16: 
+	fmt.Println("Music is a beautiful thing and its power should never be underestimated.")
+
+	// Line 17: 
+	fmt.Println("So turn it up and enjoy the ride!")
+
+	// Line 18: 
+	fmt.Println("No matter what life throws at you, you can always depend on music to be there for you.")
+
+	// Line 19: 
+	fmt.Println("So let the power of music bring a little extra joy and positivity into your life.")
+
+	// Line 20: 
+	fmt.Println("Once you experience it, you will never forget it!")
+
+	// Line 21: 
+	fmt.Println("Music has the power to bring us together, to inspire us, to give us hope, and to create memories that will last a lifetime.")
+
+	// Line 22: 
+	fmt.Println("So whether you’re a musician or a listener, take the time to appreciate the power of music in your life.")
+
+	// Line 23: 
+	fmt.Println("And if you ever need a break from the chaos, just close your eyes and let the music take you away.")
+
+	// Line 24: 
+	fmt.Println("The power of music is beyond words, so don’t miss out on the opportunity to be moved by it.")
+
+	// Line 25: 
+	fmt.Println("Turn on your favorite song and just let it take you away!")
+
+	// Line 26: 
+	fmt.Println("Music can soothe your soul, make you feel alive, and bring a little extra joy to your life.")
+
+	// Line 27: 
+	fmt.Println("It’s a powerful force for good and its power should never be underestimated.")
+
+	// Line 28: 
+	fmt.Println("So remember to take a break from the chaos and surround yourself with your favorite tunes.")
+
+	// Line 29: 
+	fmt.Println("You never know what magical moments music can bring!")
+
+	// Line 30: 
+	fmt.Println("The power of music is unmatched and its power should never be taken for granted.")
+
+	// Line 31: 
+	fmt.Println("So put on your favorite song and just let the music take you away!")
+
+	// Line 32: 
+	fmt.Println("From jazz, pop, hip-hop, classical, rock, and more, the power of music is something that can transcend barriers of language, race, and culture.")
+
+	// Line 33: 
+	fmt.Println("It has the power to move us in ways that nothing else can, so don’t forget to take a break and enjoy the magic of music." )
+
+	// Line 34: 
+	fmt.Println("It can bring us joy, it can bring us comfort, and it can bring us together.")
+
+	// Line 35: 
+	fmt.Println("No matter what genre, music has the power to inspire, motivate, and uplift us in the most magical way.")
+
+	// Line 36: 
+	fmt.Println("So take a break from the chaos and let the power of music work wonders on your soul.")
+
+	// Line 37: 
+	fmt.Println("No matter what life throws at you, you can always depend on music to be there for you.")
+
+	// Line 38: 
+	fmt.Println("From inspiring us to creating moments we’ll never forget, the power of music is undeniable.")
+
+	// Line 39: 
+	fmt.Println("So if you ever need a reminder of the power of music, just close your eyes and let it take you away.")
+
+	// Line 40: 
+	fmt.Println("And don’t forget, the power of music is beautiful, so crank up the volume and let the music wash over you!")
+
+	// Line 41: 
+	fmt.Println("The power of music can bring us together, motivate us, and bring us joy.")
+
+	// Line 42: 
+	fmt.Println("It can also bring us comfort and hope in moments of sadness, and create memories that will last a lifetime.")
+
+	// Line 43: 
+	fmt.Println("Music has the power to move us in ways nothing else can, and its power should never be underestimated.")
+
+	// Line 44: 
+	fmt.Println("So always turn up the volume, and let the music take you away!")
+
+	// Line 45: 
+	fmt.Println("No matter your mood, you can always count on music to make it better.")
+
+	// Line 46: 
+	fmt.Println("The power of music is undeniable, so don’t forget to take a break and enjoy your favorite tunes.")
+
+	// Line 47: 
+	fmt.Println("Just close your eyes and let the music take you away to a different time and place!")
+
+	// Line 48: 
+	fmt.Println("The power of music can bring us together, inspire us, give us hope, and create memories that will last a lifetime.")
+
+	// Line 49: 
+	fmt.Println("So don’t forget to crank up the volume and let the music do its magic!")
+
+	// Line 50: 
+	fmt.Println("Music has the power to move us in ways nothing else can. So don’t miss out on the opportunity to be moved by it!")
+}
